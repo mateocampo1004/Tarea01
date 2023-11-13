6cf2aadecafbb323d6141af71f435a2b776d020c
mateocampo1004 <146900595+mateocampo1004@users.noreply.github.com>
2023-11-13 16:32:18 -0500

    bucles completo

--- bucles/bucles.js
+++ bucles/bucles.js
@@ -1,7 +1,22 @@
+//Mostrar los 5 primeros numeros mediante bucle
 
-let contador = 1
-
+//while
+let contador = 1;
 while (contador <= 5);
     alert(contador);
     contador++; 
-alert("Bucle completado.")
+alert("Bucle completado.");
+//do-while
+
+let cont = 1;
+do {
+    alert(cont);
+    cont++; 
+} while (cont <= 5);
+
+alert("Bucle completado.");
+//for 
+for (let conta1 = 1; conta1 <= 5; conta1++) {
+    alert(contador);
+}
+alert("Bucle completado.");
